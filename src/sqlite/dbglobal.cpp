e1019e004509517956be809d91975dbf5373d0d4
scofieldzhu <scofieldzhu001@163.com>
2019-02-14 22:22:04 +0800

    add dbglobal module files

---
+++ src/sqlite/dbglobal.cpp
@@ -0,0 +1,20 @@
+/*=======================================================================
+Ratel is a application framework, which provides some convenient librarys
+for for those c++ developers pursuing fast-developement.
+Copyright (c) scofieldzhu. All rights reserved.	
+
+Project: ratel.sqlite 
+Module: dbglobal.cpp 
+CreateTime: 2019-1-26 19:52
+=========================================================================*/
+#include "dbglobal.h"
+
+RATEL_NAMESPACE_BEGIN
+namespace sqlkw
+{
+    const RString kPrimaryKey = "PRIMARY KEY";
+    const RString kUnique = "UNIQUE";
+    const RString kNotNull = "NOT NULL";
+    const RString kDefault = "DEFAULT";
+}
+RATEL_NAMESPACE_END
---
+++ src/sqlite/dbglobal.h
@@ -0,0 +1,26 @@
+/*=======================================================================
+Ratel is a application framework, which provides some convenient librarys
+for for those c++ developers pursuing fast-developement.
+Copyright (c) scofieldzhu. All rights reserved.	
+
+Project: ratel.sqlite 
+Module: dbglobal.h 
+CreateTime: 2019-1-26 19:42
+=========================================================================*/
+#ifndef __dbconstant_h__
+#define __dbconstant_h__
+
+#include "sqlitepublic.h"
+
+RATEL_NAMESPACE_BEGIN
+
+namespace sqlkw
+{
+    RATEL_SQLITE_API extern const RString kPrimaryKey;
+    RATEL_SQLITE_API extern const RString kUnique;
+    RATEL_SQLITE_API extern const RString kNotNull;
+    RATEL_SQLITE_API extern const RString kDefault;
+}
+
+RATEL_NAMESPACE_END
+#endif
