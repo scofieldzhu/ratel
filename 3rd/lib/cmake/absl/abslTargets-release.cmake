dde6a9c3646822678399d33270fdb7222cfa73e1
scofieldzhu <scofieldzhu@qq.com>
2023-12-11 19:42:11 +0800

    更新3rd目录结构、添加protobuf编译库，更新cmakelist引用

---
+++ 3rd/lib/cmake/absl/abslTargets-release.cmake
@@ -0,0 +1,159 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "absl::flags_program_name" for configuration "Release"
+set_property(TARGET absl::flags_program_name APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_program_name PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_program_name.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_program_name )
+list(APPEND _cmake_import_check_files_for_absl::flags_program_name "${_IMPORT_PREFIX}/lib/absl_flags_program_name.lib" )
+
+# Import target "absl::flags_config" for configuration "Release"
+set_property(TARGET absl::flags_config APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_config PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_config.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_config )
+list(APPEND _cmake_import_check_files_for_absl::flags_config "${_IMPORT_PREFIX}/lib/absl_flags_config.lib" )
+
+# Import target "absl::flags_marshalling" for configuration "Release"
+set_property(TARGET absl::flags_marshalling APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_marshalling PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_marshalling.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_marshalling )
+list(APPEND _cmake_import_check_files_for_absl::flags_marshalling "${_IMPORT_PREFIX}/lib/absl_flags_marshalling.lib" )
+
+# Import target "absl::flags_commandlineflag_internal" for configuration "Release"
+set_property(TARGET absl::flags_commandlineflag_internal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_commandlineflag_internal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_commandlineflag_internal.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_commandlineflag_internal )
+list(APPEND _cmake_import_check_files_for_absl::flags_commandlineflag_internal "${_IMPORT_PREFIX}/lib/absl_flags_commandlineflag_internal.lib" )
+
+# Import target "absl::flags_commandlineflag" for configuration "Release"
+set_property(TARGET absl::flags_commandlineflag APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_commandlineflag PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_commandlineflag.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_commandlineflag )
+list(APPEND _cmake_import_check_files_for_absl::flags_commandlineflag "${_IMPORT_PREFIX}/lib/absl_flags_commandlineflag.lib" )
+
+# Import target "absl::flags_private_handle_accessor" for configuration "Release"
+set_property(TARGET absl::flags_private_handle_accessor APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_private_handle_accessor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_private_handle_accessor.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_private_handle_accessor )
+list(APPEND _cmake_import_check_files_for_absl::flags_private_handle_accessor "${_IMPORT_PREFIX}/lib/absl_flags_private_handle_accessor.lib" )
+
+# Import target "absl::flags_reflection" for configuration "Release"
+set_property(TARGET absl::flags_reflection APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_reflection PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_reflection.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_reflection )
+list(APPEND _cmake_import_check_files_for_absl::flags_reflection "${_IMPORT_PREFIX}/lib/absl_flags_reflection.lib" )
+
+# Import target "absl::flags_internal" for configuration "Release"
+set_property(TARGET absl::flags_internal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_internal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_internal.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_internal )
+list(APPEND _cmake_import_check_files_for_absl::flags_internal "${_IMPORT_PREFIX}/lib/absl_flags_internal.lib" )
+
+# Import target "absl::flags" for configuration "Release"
+set_property(TARGET absl::flags APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags )
+list(APPEND _cmake_import_check_files_for_absl::flags "${_IMPORT_PREFIX}/lib/absl_flags.lib" )
+
+# Import target "absl::flags_usage_internal" for configuration "Release"
+set_property(TARGET absl::flags_usage_internal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_usage_internal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_usage_internal.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_usage_internal )
+list(APPEND _cmake_import_check_files_for_absl::flags_usage_internal "${_IMPORT_PREFIX}/lib/absl_flags_usage_internal.lib" )
+
+# Import target "absl::flags_usage" for configuration "Release"
+set_property(TARGET absl::flags_usage APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_usage PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_usage.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_usage )
+list(APPEND _cmake_import_check_files_for_absl::flags_usage "${_IMPORT_PREFIX}/lib/absl_flags_usage.lib" )
+
+# Import target "absl::flags_parse" for configuration "Release"
+set_property(TARGET absl::flags_parse APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::flags_parse PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_flags_parse.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::flags_parse )
+list(APPEND _cmake_import_check_files_for_absl::flags_parse "${_IMPORT_PREFIX}/lib/absl_flags_parse.lib" )
+
+# Import target "absl::log_flags" for configuration "Release"
+set_property(TARGET absl::log_flags APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::log_flags PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_log_flags.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::log_flags )
+list(APPEND _cmake_import_check_files_for_absl::log_flags "${_IMPORT_PREFIX}/lib/absl_log_flags.lib" )
+
+# Import target "absl::string_view" for configuration "Release"
+set_property(TARGET absl::string_view APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::string_view PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/absl_string_view.lib"
+  )
+
+list(APPEND _cmake_import_check_targets absl::string_view )
+list(APPEND _cmake_import_check_files_for_absl::string_view "${_IMPORT_PREFIX}/lib/absl_string_view.lib" )
+
+# Import target "absl::abseil_dll" for configuration "Release"
+set_property(TARGET absl::abseil_dll APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(absl::abseil_dll PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/abseil_dll.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/abseil_dll.dll"
+  )
+
+list(APPEND _cmake_import_check_targets absl::abseil_dll )
+list(APPEND _cmake_import_check_files_for_absl::abseil_dll "${_IMPORT_PREFIX}/lib/abseil_dll.lib" "${_IMPORT_PREFIX}/bin/abseil_dll.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
